89da39999e079c3b50507461a2b00b3bd992c2af
Alistair_Piot <alistair.pro15@gmail.com>
2024-03-26 21:02:29 +0100

    ajout de la cover pour chaque vignette de logement

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,17 +1,23 @@
 import "./_card.scss";
 import { Link } from "react-router-dom";
-import Data from "../../data/data.json";
+// import Data from "../../data/data.json";
+import PropTypes from "prop-types";
 
-function Card() {
+function Card({ data }) {
     return (
         <div className="card-container">
-            {Data.map((post) => {
+            {data.map((post) => {
                 return (
                     <Link
                         className="card-container__card"
                         key={post.id}
                         to={`/fiche-logement/${post.id}`}
                     >
+                        <img
+                            className="card-container__card__cover"
+                            src={post.cover}
+                            alt={post.title}
+                        ></img>
                         <h3 className="card-container__card__title">
                             {post.title}
                         </h3>
@@ -21,5 +27,15 @@ function Card() {
         </div>
     );
 }
+
+Card.propTypes = {
+    data: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            title: PropTypes.string.isRequired,
+            cover: PropTypes.string.isRequired,
+        })
+    ).isRequired,
+};
 
 export default Card;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,13 +3,14 @@ import Card from "../../components/card/Card";
 import "../../sass/base/_normalize.scss";
 import "./_home.scss";
 import BannerImage from "../../assets/images/image-1.png";
+import Data from "../../data/data.json";
 
 function Home() {
     return (
         <div>
             <Banner src={BannerImage} title="Chez vous, partout ailleurs" />
             <div className="page-container__gallery">
-                <Card />
+                <Card data={Data} />
             </div>
         </div>
     );
